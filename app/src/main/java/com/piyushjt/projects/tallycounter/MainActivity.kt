ea12112ee26b1a2ccf2054decd796988495f4b70
Piyush_jt__ <piyushthummar396@gmail.com>
2023-11-28 13:23:31 +0530

    made UI and Logic and added vibration haptics on buttons

--- app/src/main/java/com/piyushjt/projects/tallycounter/MainActivity.kt
+++ app/src/main/java/com/piyushjt/projects/tallycounter/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.piyushjt.projects.tallycounter
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import com.piyushjt.projects.tallycounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val noText= binding.count
+
+        binding.addCount.setOnClickListener {
+            noText.text= count()
+            vibe()
+        }
+
+        binding.reset.setOnClickListener {
+            reset()
+            vibe()
+        }
+
     }
+
+    fun count(): String{
+        val noText= binding.count.text.toString().toInt()
+        return "${noText+1}"
+    }
+
+    fun reset(){
+        val noText= binding.count
+        noText.text = "0"
+    }
+
+    fun vibe(){
+        val vibrate= getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        vibrate.vibrate(VibrationEffect.createOneShot(55, VibrationEffect.DEFAULT_AMPLITUDE))
+    }
+
+    fun vibeLong(){
+        val vibrate= getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        vibrate.vibrate(VibrationEffect.createOneShot(75, VibrationEffect.DEFAULT_AMPLITUDE))
+    }
+
 }
